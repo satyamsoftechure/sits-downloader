27115a96432d701ec7ab5d42a2b47bf7af2a0c4e
satyamsoftechure <153186885+satyamsoftechure@users.noreply.github.com>
2024-10-04 14:31:06 +0530

    Update serializers.py

--- downloader/serializers.py
+++ downloader/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 
 class URLSerializer(serializers.Serializer):
     url = serializers.URLField()
-    format_type = serializers.CharField(required=False, default='all')
 
 class FormatSerializer(serializers.Serializer):
     format_id = serializers.CharField()
