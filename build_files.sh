3afb6228763243c9f5575c7b6e45ec93f4214976
satyamsoftechure <153186885+satyamsoftechure@users.noreply.github.com>
2024-10-04 16:16:22 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -4,3 +4,8 @@ echo "Build end"
 echo "Build start"
 python3.12 manage.py collectstatic --noinput --clear
 echo "Build end"
+echo "Downloading FFmpeg..."
+mkdir -p bin
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz | tar -xJ --strip-components=1 -C ./bin
+chmod +x ./bin/ffmpeg
+echo "FFmpeg installed successfully."
